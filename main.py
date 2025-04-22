577fcc2334d14dcb7008ef1640d2cf23f6468401
krishie411 <krishie411@gmail.com>
2025-04-23 01:54:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ os.environ['GOOGLE_API_KEY'] = st.secrets['GOOGLE_API_KEY']
 
 # Create prompt template for generating tweets
 
-tweet_template = "Give me {number} tweets on {topic}"
+tweet_template = "Give me {number} distinct characteristics of {raga} raga"
 
-tweet_prompt = PromptTemplate(template = tweet_template, input_variables = ['number', 'topic'])
+tweet_prompt = PromptTemplate(template = tweet_template, input_variables = ['number', 'raga'])
 
 # Initialize Google's Gemini model
 gemini_model = ChatGoogleGenerativeAI(model = "gemini-1.5-flash-latest")
@@ -23,15 +23,15 @@ tweet_chain = tweet_prompt | gemini_model
 
 import streamlit as st
 
-st.header("Tweet Generator - SATVIK")
+st.header("Raga Info Generator - Sinnakkrishnan")
 
-st.subheader("Generate tweets using Generative AI")
+st.subheader("Generate Info Bytes using Generative AI")
 
-topic = st.text_input("Topic")
+Raga = st.text_input("Raga")
 
-number = st.number_input("Number of tweets", min_value = 1, max_value = 10, value = 1, step = 1)
+number = st.number_input("Number of Info Bytes", min_value = 1, max_value = 10, value = 1, step = 1)
 
 if st.button("Generate"):
-    tweets = tweet_chain.invoke({"number" : number, "topic" : topic})
+    tweets = tweet_chain.invoke({"number" : number, "raga" : Raga})
     st.write(tweets.content)
     
